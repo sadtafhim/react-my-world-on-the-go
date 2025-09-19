97a19623c0f7f59ebee4972b8856eb688da16bce
Saad <tafhim.sad@gmail.com>
2025-09-19 22:37:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries.jsx/Countries";
+
+function App() {
+  return (
+    <>
+      <h1>My world on the go..</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
